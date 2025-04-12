225f69aa4071e2d637241a4919050cd28c872949
kelvin <kelvin.oy16@gmail.com>
2025-04-12 17:55:06 +0000

    Added Animations

--- src/pages/Animate.jsx
+++ src/pages/Animate.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { useRef } from 'react'
-import { div } from 'framer-motion/client';
+import Ebike from  '../assets/images/bikeBlack.png'
+import Hero from '../components/Hero'
 
 function Animate() {
     const ref = useRef(null);
-
-    const {scrollYProgress} = useScroll({
+    const {scrollYProgress:scrollYProgress1} = useScroll({
         target: ref,
         offset: ["center center", "end start"]
     })
+    const scale = useTransform(scrollYProgress1, [0, 1], [0.5, 20.5]);
 
-    const scale = useTransform(scrollYProgress, [0, 1], [0.5, 50.5]);
+    
+    const {scrollYProgress:scrollYProgress2} = useScroll({
+        target: ref,
+        offset: ["center center", "end start"]
+    })
+    const scale2 = useTransform(scrollYProgress2, [0, 1], [0.5, 50.5]);
+
+    const ref3 = useRef(null);
+    const {scrollYProgress:scrollYProgress3} = useScroll({
+        target: ref3,
+        offset: ["center center", "end start"]
+    })
+    const scale3 = useTransform(scrollYProgress3, [0, 1], [1, 0.8])
+    const borderRadius = useTransform(scrollYProgress3, [0, 1], ['0px', '150px'])
+
+
   return (
-    <div>
-        <div className='text-center'>
+    <div className='relative'>
+        <div className='absolute left-[20%] text-center'>
             <h1 className='text-5xl font-semibold mb-10'>What Our E-bike Offers</h1>
 
-            <p className='text-[20px] text-[#777]'>From getting to class on time to cruising around campus with ease, our e-bikes are built to make student life simpler, faster, and way more fun. <br /> Check out all the cool features that make riding with us a no-brainer.</p>
+            <p className='text-[20px] text-[#777]'>
+                From getting to class on time to cruising around campus with ease, our e-bikes are built to make student life simpler, faster, and way more fun. <br /> Check out all the cool features that make riding with us a no-brainer.
+            </p>
         </div>
         <div ref={ref} className=' relative w-full h-[200vh] flex justify-center items-center overflow-hidden font-helvetica'>
             
@@ -42,11 +60,13 @@ function Animate() {
                 </div>
                 
             </div>
-            <motion.div className='z-5 bg-[#333] w-[350px] h-[350px] rounded-[170px] flex justify-center items-center' style={{scale}}>
-                <div className='bg-white w-[50px] h-[50px] rounded-4xl'>
+
+            <motion.div className='z-5 bg-[#000] w-[350px] h-[350px] rounded-[170px] flex justify-center items-center' style={{scale}}>
+                <motion.div style={{scale2}} className='bg-white w-[50px] h-[50px] rounded-4xl'>
                     
-                </div>
+                </motion.div>
             </motion.div>
+
             <div className='absolute right-100 flex flex-col space-y-40 w-[500px]'>
                 <div className='border-b border-[#ddd] border-l rounded-bl-[140px] pr-5 pb-5 h-[500px] flex justify-center items-center'>
                     <div className='px-10'>
@@ -66,15 +86,28 @@ function Animate() {
                     </div>
                     
                 </div>
-                
             </div>
+        </div>
+        <motion.div className=' bg-black w-full py-10' ref={ref3} style={{scale: scale3, borderRadius}}>
+            <div className='max-w-[1400px] mx-auto bg-[#000] py-10 rounded-[70px]'>
+                <div className='w-full text-center '>
+                    <h1 className='text-white text-[40px] mb-5'>
+                        We're Coming to You Soon
+                    </h1>
+                    <p className='text-[#888] text-[18px]'>
+                        We're starting on campus, but you're definitely not left out. <br /> Our e-bike program is gearing up to roll out to the general public—so get ready to ride with us soon!
+                    </p>
+                </div>
 
-            
-      
-        </div>
-        <div className='h-[100vh] bg-black'>
-                <p className='text-white'>Some Content</p>
-        </div>
+                <div className='w-full flex justify-center items-center'>
+                    <img src={Ebike} alt="Electric Bike" className='w-170'/>
+                </div>
+
+                <button></button>
+            </div>
+        </motion.div>
+        <Hero />
+        <Hero />
     </div>
     
   )
