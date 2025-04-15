fb54c32f81113e4be94e17d11b8a89f818be2c32
kelvin <kelvin.oy16@gmail.com>
2025-04-15 09:36:50 +0000

    Responsiveness

--- src/pages/NewFeatures.jsx
+++ src/pages/NewFeatures.jsx
@@ -6,11 +6,11 @@ import DropOff from '../assets/images/Dropoff.jpeg'
 
 function NewFeatures() {
   return (
-    <div className='w-full bg-black text-white py-20 mt-0 font-helvetica px-10 sm:bg-amber-300 md:bg-green-400 lg:bg-blue-500'>
+    <div className='w-full bg-black text-white py-20 mt-0 font-helvetica px-10'>
       <div className='max-w-[1200px] mx-auto'>
-        <h1 className='text-[28px] font-semibold'>What's New?</h1>
+        <h1 className='text-[28px] font-semibold md:text-[42px] lg:text-[52px]'>What's New?</h1>
         <div className='mt-10'>
-            <div  className='flex flex-col justify-center items-center gap-x-30 mb-15'>
+            <div  className='flex flex-col justify-center items-center gap-x-30 mb-15 lg:flex-row md:mb-30 lg:mb-40'>
                 <div className='w-full sm:h-[350px] md:h-[400px]'>
                     <img src={manAnimate} alt="Man in couch" className='w-100 rounded-4xl sm:w-200 sm:object-cover sm:h-full md:w-300 md:h-full'/>
                 </div>
@@ -19,13 +19,56 @@ function NewFeatures() {
                     <p className='text-[17px] md:text-[18px]'>
                         Plan ahead with ease - our new Ride Scheduling feature lets you book your trips in advance, so you're alwayas on time, every, time.
                     </p>
-                    <div className='md:w-full md:flex md:justify-center'>
+                    <div className='md:w-full md:flex md:justify-center lg:block'>
                         <GetStartedBtn />
                     </div>
                 </div>
             </div>
 
-            <div  className='flex flex-col justify-center items-center gap-y-10 mb-15'>
+            <div  className='flex flex-col justify-center items-center gap-x-30 mb-15 lg:flex-row md:mb-30 lg:mb-40'>
+                <div className='w-full sm:h-[350px] md:h-[400px] lg:w-[2000px]'>
+                    <img src={Women} alt="Man in couch" className='w-100 rounded-4xl sm:w-200 sm:object-cover sm:h-full md:w-300 md:h-full'/>
+                </div>
+                <div>
+                    <h1 className='text-[28px] font-regular my-8'>Ride Scheduling</h1>
+                    <p className='text-[17px] md:text-[18px]'>
+                        For added security, every ride now comes with a unique 4-digit Safety PIN. Before your trip begins, share this PIN with your driver to ensure you're in the right ride. This helps prevent ride mix-ups and enhances passenger safety.
+                    </p>
+                    <div className='md:w-full md:flex md:justify-center lg:block'>
+                        <GetStartedBtn />
+                    </div>
+                </div>
+            </div>
+
+            
+
+            <div  className='flex flex-col justify-center items-center gap-x-30 mb-15 lg:flex-row '>
+                <div className='w-full sm:h-[350px] md:h-[400px] lg:w-[2000px]'>
+                    <img src={DropOff} alt="Man in couch" className='w-100 rounded-4xl sm:w-200 sm:object-cover sm:h-full md:w-300 md:h-full'/>
+                </div>
+                <div>
+                    <h1 className='text-[28px] font-regular my-8'>LuxeDrop</h1>
+                    <p className='text-[17px] md:text-[18px]'>
+                        Need to send a package quickly? LuxeDrop lets you book a driver to deliver your parcels, documents, or small items securely and on time. Whether it's a last-minute delivery or a planned drop-off, LuxeDrop ensures fast and reliable service.
+                    </p>
+                    <div className='md:w-full md:flex md:justify-center lg:block'>
+                        <GetStartedBtn />
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewFeatures
+
+
+
+{/* <div  className='flex flex-col justify-center items-center gap-y-10 mb-15'>
                 <div>
                     <h1 className='text-[28px] font-regular my-8'>Safety PIN</h1>
                     <p className='text-[18px]'>
@@ -49,11 +92,4 @@ function NewFeatures() {
                     </p>
                     <GetStartedBtn />
                 </div>
-            </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default NewFeatures
+            </div> */}
