3b76f198d0e247c5a32538ae53b0c9fc44aaf3f3
kelvin <kelvin.oy16@gmail.com>
2025-04-16 10:55:49 +0000

    Worked on responsiveness

--- src/components/AppDownload.jsx
+++ src/components/AppDownload.jsx
@@ -5,7 +5,7 @@ import Playstore from '../assets/icons/playstore.png'
 import Mockup from '../assets/images/mockup.png'
 function AppDownload() {
   return (
-    <div className='w-full  pt-20 font-SFPro px-5 '>
+    <div className='w-full  pt-20 font-SFPro px-5 overflow-hidden'>
         <div className='max-w-[1400px] mx-auto px-0 lg:px-10 md:bg-yellow-400 md:py-5'>
             <div className='flex flex-col lg:flex-row justify-between items-center gap-y-4 h-full lg:h-[85vh]'>
                 <div>
