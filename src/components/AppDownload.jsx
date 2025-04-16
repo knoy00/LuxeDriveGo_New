b908eafbc9f7ff584cf1fc9eb01bd6950eea7a0f
kelvin <kelvin.oy16@gmail.com>
2025-04-16 11:25:28 +0000

    Worked on responsiveness

--- src/components/AppDownload.jsx
+++ src/components/AppDownload.jsx
@@ -5,14 +5,14 @@ import Playstore from '../assets/icons/playstore.png'
 import Mockup from '../assets/images/mockup.png'
 function AppDownload() {
   return (
-    <div className='w-full  pt-20 font-SFPro px-5'>
+    <div className='w-full  pt-20 font-SFPro px-5 sm:bg-amber-300'>
         <div className='max-w-[1400px] mx-auto px-0 lg:px-10 md:py-5'>
-            <div className='h-[95vh] flex flex-col lg:flex-row justify-between items-center gap-y-4 lg:h-[85vh]'>
+            <div className='h-screen flex flex-col lg:flex-row justify-between items-center gap-y-4 lg:h-[85vh]'>
                 <div className=''>
                     <h1 className=' text-center text-[40px] sm:w-full lg:text-[52px] font-semibold sm:text-center lg:text-left lg:max-w-[450px]'>Download Our Mobile App.</h1>
                     <p className='text-center text-[17px] sm:text-center lg:max-w-[350px] mt-5 lg:h-[100px] lg:w-[500px] sm:w-full text-[#999] lg:text-left'>Discover the convenience of our mobile app, designed for a seamless and hassle-free experience.</p>
 
-                    <div className='flex flex-col lg:flex-row justify-between items-center mt-10 gap-y-5 lg:gap-x-10'>
+                    <div className='flex flex-col lg:flex-row justify-between items-center mt-10 gap-y-5 lg:gap-x-10 sm:flex-row sm:gap-x-10 sm:justify-center'>
                         <div className='flex items-center gap-x-3  border-2 border-black rounded-2xl px-5 py-3 w-[250px] max-w-[250px] lg:gap-x-5 cursor-pointer hover:bg-[#dfdfdfb2] sm:gap-x-3'>
                             <img src={Apple} alt="Apple" />
                             <div>
